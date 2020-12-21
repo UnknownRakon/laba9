1c9c0f34d6cb5039811926a2227be00240446216
Vlad <gorenfeo@gmail.com>
2020-12-21 21:18:45 +0300

    two

--- main.js
+++ main.js
@@ -11,3 +11,16 @@ let myCar = {
 console.log('Свойства: ', Object.keys(myCar).toString());
 console.log('Вы продаёте ' + myCar.color + ' ' + myCar.carName + ' ' + myCar.year + ' года выпуска, ' + 'мощностью ' + myCar.horsePowers + ' за ' + myCar.cost + 'рублей');
 
+// Задание 2
+console.log("Задание 2");
+
+let array = [5, 4, 3, 2, 1]; 
+console.log(`Начальный массив ${array.join(', ')}`)
+function newArray(array) {
+   for(let i = array.length; i > 0; i--) {
+       if(i != 1) { 
+           console.log(`Удалили ${array.pop()}, осталось ${array.join(', ')}`);
+        }
+   } 
+};
+newArray(array);
